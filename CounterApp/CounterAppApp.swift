d024f6325d894f026afc29660a18553d42a3c0d2
RyoKitatani <ryo0929m@gmail.com>
2021-05-11 00:59:25 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by RyoKitatani on 2021/05/11.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
